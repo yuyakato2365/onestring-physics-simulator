6ba8a1911214b42be5a045693012b023f80d3da8
yuyakato2365 <yjiateng273@gmail.com>
2026-09-20 16:49:27 +0900

    Enable K3D iteration history in paper T3D launcher

--- onestring-physics-simulator/app_optcuts_20260920_paper_t3d.py
+++ onestring-physics-simulator/app_optcuts_20260920_paper_t3d.py
@@ -17,9 +17,11 @@ if str(SRC) not in sys.path:
 import onestring_physics as package
 from onestring_physics import onestring_pipeline as pipeline
 from onestring_physics.paper_t3d_20260920_patch import install_paper_t3d_20260920_patch
+from onestring_physics.k3d_iteration_history_patch import install_k3d_iteration_history_patch
 
 # Numerical switch: this launcher always uses the paper T3D implementation.
 install_paper_t3d_20260920_patch(pipeline)
+install_k3d_iteration_history_patch(pipeline)
 package.onestring_pipeline = pipeline
 package.build_onestring_design = pipeline.build_onestring_design
 
